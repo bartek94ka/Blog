f0a99b3494648d3f4d6323c9d730026b0bd20740
G0ZCN12\bkaczorowski <bkaczorowski@pl.sii.eu>
2017-12-22 13:14:13 +0100

    Creating Navbar component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // template: '<navbar></navbar>',
+  // template: '<h1>template</h1>'
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular';
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
+    NavbarComponent,
     AppComponent
+
   ],
   imports: [
     BrowserModule
