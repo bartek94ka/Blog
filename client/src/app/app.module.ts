cc08f2f867fbc0121674c63a3eff1cfef7adde28
G0ZCN12\bkaczorowski <bkaczorowski@pl.sii.eu>
2018-01-04 14:42:08 +0100

    Adding routing and split view to components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component'
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ContentComponent } from './components/content/content.component';
 import { PostComponent } from './components/post/post.component';
@@ -10,9 +12,29 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { LatestPostsComponent } from './components/widgets/latestposts/latestposts.component';
 import { CategoriesComponent } from './components/widgets/categories/categories.component';
 import { TagsComponent } from './components/widgets/tags/tags.component';
+import { LoginComponent } from './components/session/login/login.component';
+import { RegisterComponent } from './components/session/register/register.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+
+
+const appRoutes: Routes = [
+  // { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+]
 
 @NgModule({
   declarations: [
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    LoginComponent,
+    RegisterComponent,
     NavbarComponent,
     AppComponent,
     ContentComponent,
@@ -24,7 +46,8 @@ import { TagsComponent } from './components/widgets/tags/tags.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
