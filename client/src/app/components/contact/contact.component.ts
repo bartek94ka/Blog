93a471e0ef81e497f6f7260f8cb8992d583c3c39
G0ZCN12\bkaczorowski <bkaczorowski@pl.sii.eu>
2018-02-02 13:59:28 +0100

    Fill about page

--- client/src/app/components/contact/contact.component.ts
+++ client/src/app/components/contact/contact.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'contact',
-  templateUrl: './contact.component.html'
-//   styleUrls: ['./app.component.css']
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
-  title = 'Angular';
+  constructor(){}
+  imageContact: string = "assets/images/contact.png";
+  imageEmail: string = "assets/images/email.png";
+  imageLinkedIn: string = "assets/images/linkedin.png";
+  imageFacebook: string = "assets/images/facebook.png";
+  imageGithub: string = "assets/images/github.png";
 }
